f5da2767519a7627a366a57d609a01f7642d56e3
Simon Garcia <simongv97@gmail.com>
2019-02-13 10:35:11 +0100

    start_v2

--- src/htmls/Pages.java
+++ src/htmls/Pages.java
@@ -1,11 +1,20 @@
 package htmls;
 
 public class Pages {
+	
+	private static String header() {
+		return "<head><title>Page title</tittle></head>";
+	}
 
 	public static String indexHtml() {
 		String html="";
 		
-		
+		html = header()
+				+ "<body>"
+				+ "<h1>Activity functionalities</h1><br/>"
+				+ ""
+				+ ""
+				+ "</body>";
 		
 		return html;
 	}
